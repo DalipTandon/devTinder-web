0bf92cf5a2e86eb8177ca65ee67b1c6caa5b08bb
Dalip Tandon <daliptandon8@gmail.com>
2024-10-21 00:03:57 +0530

    create routing,create new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import Navbar from "./navbar"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Body from "./Body"
+import Login from "./Login"
+import Profile from "./Profile"
 
 function App() {
 
  return(
   <>
-  <Navbar/> 
+  
+  <BrowserRouter basename="/">
+  <Routes>
+    <Route path="/" element={<Body/>}>
+    <Route path="/login" element={<Login/>}/>
+    <Route path="/profile" element={<Profile/>}/>
 
+    </Route>
+  </Routes>
+  </BrowserRouter>
   </>
   
  )
