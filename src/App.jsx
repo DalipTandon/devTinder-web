6e21c0096168d9eddddd1e09a94f4ed84700effb
Dalip Tandon <daliptandon8@gmail.com>
2024-10-21 13:28:13 +0530

    moved navbar to body

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-<<<<<<< HEAD
+
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Body from "./Body"
 import Login from "./Login"
 import Profile from "./Profile"
-=======
-import Navbar from "./navbar"
->>>>>>> e152c1e860f6863a1a854ef743e3131d5868657f
-
 function App() {
 
  return(
   <>
-<<<<<<< HEAD
-  
   <BrowserRouter basename="/">
   <Routes>
     <Route path="/" element={<Body/>}>
@@ -22,10 +16,6 @@ function App() {
     </Route>
   </Routes>
   </BrowserRouter>
-=======
-  <Navbar/> 
-
->>>>>>> e152c1e860f6863a1a854ef743e3131d5868657f
   </>
   
  )
